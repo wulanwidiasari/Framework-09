b3c25398684db66af439f32f9b55af11e7035ba3
Wulan <widiaawln443@gmail.com>
2022-03-08 16:16:07 +0700

    03-Tugas Framework

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Header from './component/Header';
+import Footer from './component/Footer';
+import ListImage from './component/ListImage'; 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <h1>Component dari Class App</h1>
+        <ListImage />
+        <Footer judul='Halaman Footer' nama='Aufa' />
+      </div>
+    );
+  }
+} export default App;
---
+++ src/component/HelloComponent.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './HelloComponent.css';
+
+// function HelloComponent() {
+//     return HelloComponent;
+// }
+
+const HelloComponent = () => {
+    return <p className="text-p">Ini adalah arrow function yang ada di folder</p>
+}
+
+export default HelloComponent; //agar component ini bisa di pakai dimana saja

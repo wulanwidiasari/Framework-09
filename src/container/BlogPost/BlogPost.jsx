0bef46f16566dc40d522ec5050301ac70a95f480
Wulan <widiaawln443@gmail.com>
2022-03-21 07:34:48 +0700

    Praktikum 04

--- src/container/BlogPost/BlogPost.jsx
+++ src/container/BlogPost/BlogPost.jsx
@@ -43,7 +43,12 @@ import Post from "../../component/BlogSpot/Post";
 
 class BlogPost extends Component{
     state ={                    // komponen state dari React untuk statefull component
-        listArtikel: []         // variabel arrat yang digunakan untuk menyimpan data API
+        listArtikel: [],         // variabel arrat yang digunakan untuk menyimpan data API
+        insertArtikel: [],
+            userId:1,
+            id:1,
+            title:"",
+            body:""
     }
 
     ambilDataDariServerAPI=()=>{
@@ -66,9 +71,46 @@ class BlogPost extends Component{
             this.ambilDataDariServerAPI()
         })
     }
+    handleTambahArtikel=(event)=>{
+        let formInsertArtikel={...this.state.insertArtikel};
+        let timestamp = new Date().getTime();
+        formInsertArtikel['id'] = timestamp;
+        formInsertArtikel[event.target.name] = event.target.value;
+        this.setState({
+            insertArtikel:formInsertArtikel
+        });
+    }
+    handleTombolSimpan=()=>{
+        fetch('http://localhost:3001/posts',{
+            method:'post',
+            headers:{
+                'Accept':'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(this.state.insertArtikel)
+        })
+            .then((Response)=>{
+               this.ambilDataDariServerAPI(); 
+            });
+    }
     render(){
         return(
             <div className="post-artikel">
+                <div className="form pb-2 border-buttom">
+                    <div className="form-group row">
+                        <label htmlFor="title" className="col-sm-2 col-form-label">Judul</label>
+                        <div className="col-sm-10">
+                            <input type="text" className="form-control" id="title" name="title" onChange={this.handleTambahArtikel}/>
+                        </div>
+                    </div>
+                    <div className="form-group row">
+                        <label htmlFor="body" className="col-sm-2 col-form-label">Isi</label>
+                        <div className="col-sm-10">
+                            <textarea className="form-control" id="body" name="body" rows="3" onChange={this.handleTambahArtikel}></textarea>
+                        </div>
+                    </div>
+                    <button type="submit" className="btn btn-primary" onClick={this.handleTombolSimpan}>Simpan</button>
+                </div>
                 <h2>Daftar Artikel</h2>
                 {
                     this.state.listArtikel.map(artikel =>{
